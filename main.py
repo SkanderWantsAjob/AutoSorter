29b84c74f3fa3394b05284cac243299c3e6dfef4
skander Ben achour <benachour.skander1@gmail.com>
2025-02-11 23:04:30 +0100

    more extentions and documents too

--- main.py
+++ main.py
@@ -5,19 +5,36 @@ import logging
 import colorlog
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Directories
-source_dir = "C:/Users/benac/Downloads"
-dest_dir_sfx = "C:/Users/benac/Downloads/Sound"
-dest_dir_music = "C:/Users/benac/Downloads/Sound/music"
-dest_dir_video = "C:/Users/benac/Downloads/Downloaded Videos"
-dest_dir_image = "C:/Users/benac/Downloads/downloaded Images"
+source_dir = os.getenv('SOURCE_DIR')
+dest_dir_sfx = os.getenv('DEST_DIR_SFX')
+dest_dir_music = os.getenv('DEST_DIR_MUSIC')
+dest_dir_video = os.getenv('DEST_DIR_VIDEO')
+dest_dir_image = os.getenv('DEST_DIR_IMAGE')
+dest_dir_documents = os.getenv('DEST_DIR_DOCUMENTS')
 
 os.makedirs(dest_dir_sfx, exist_ok=True)
 os.makedirs(dest_dir_music, exist_ok=True)
 os.makedirs(dest_dir_video, exist_ok=True)
 os.makedirs(dest_dir_image, exist_ok=True)
+os.makedirs(dest_dir_documents, exist_ok=True)
+
+
+#Configure extentions
+image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
+                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+# ? supported Video types
+video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+# ? supported Audio types
+audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+# ? supported Document types
+document_extensions = [".doc", ".docx", ".odt",
+                    ".pdf", ".xls", ".xlsx", ".ppt", ".pptx", "txt"]
 
 # Configure colored logging using colorlog
 
@@ -75,6 +92,35 @@ def move(dest, entry, name):
         logger.exception(f"Unexpected error moving file {entry.name}: {e}")
 
 class MoverHandler(FileSystemEventHandler):
+    def check_audio_files(self, entry, name):  # * Checks all Audio Files
+        for audio_extension in audio_extensions:
+            if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
+                if entry.stat().st_size < 2_000_000 or "SFX" in name:  # ? 2Megabytes
+                    dest = dest_dir_sfx
+                else:
+                    dest = dest_dir_music
+                move(dest, entry, name)
+                logging.info(f"Moved audio file: {name}")
+    
+    def check_video_files(self, entry, name):  # * Checks all Video Files
+        for video_extension in video_extensions:
+            if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+                move(dest_dir_video, entry, name)
+                logging.debug(f"Moved video file: {name}")
+
+    def check_image_files(self, entry, name):  # * Checks all Image Files
+        for image_extension in image_extensions:
+            if name.endswith(image_extension) or name.endswith(image_extension.upper()):
+                move(dest_dir_image, entry, name)
+                logging.debug(f"Moved image file: {name}")
+
+    def check_document_files(self, entry, name):  # * Checks all Document Files
+        for documents_extension in document_extensions:
+            if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
+                move(dest_dir_documents, entry, name)
+                logging.debug(f"Moved document file: {name}")
+    
+
     def on_modified(self, event):
         """Triggered when files are modified"""
         time.sleep(1)  # Delay to prevent moving incomplete files
@@ -87,21 +133,10 @@ class MoverHandler(FileSystemEventHandler):
                 
                 name = entry.name
                 dest = None
-
-                if name.endswith(('.wav', '.mp3')):
-                    logger.info(f"{name} -> Detected as audio file")
-                    dest = dest_dir_sfx if entry.stat().st_size < 25_000_000 or "SFX" in name else dest_dir_music
-                elif name.endswith(('.mov', '.mp4')):
-                    logger.info(f"{name} -> Detected as video file")
-                    dest = dest_dir_video
-                elif name.endswith(('.jpg', '.jpeg', '.png')):
-                    logger.info(f"{name} -> Detected as image file")
-                    dest = dest_dir_image
-
-                if dest:
-                    if is_file_complete(entry.path):
-                        move(dest, entry, name)
-                        logger.debug(f"Moved {name} to {dest}")
+                self.check_audio_files(entry, name)
+                self.check_document_files(entry, name)
+                self.check_image_files(entry, name)
+                self.check_video_files(entry, name)
 
 if __name__ == "__main__":
     observer = Observer()
